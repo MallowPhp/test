4e2af5e55a08cd167fe94865908d7dd82a51cae0
Mallow_Php <php@mallow-tech.com>
2016-01-22 17:37:04 +0530

    After making changes for cross platform

--- src/Commands/AutomateTestWithoutDbCommand.php
+++ src/Commands/AutomateTestWithoutDbCommand.php
@@ -41,13 +41,27 @@ class AutomateTestWithoutDbCommand extends Command{
         try{
             $sourceDir = __DIR__."/../testing";
             $destinationDir = "config/testing";
-            $success = File::copyDirectory($sourceDir, $destinationDir);
-            if($success){
-                $this->info("Testing directory for local database( Temporary Storage ) is successfully created");
+            $testcase_main = "tests/TestCase.php";
+            if(File::exists($testcase_main)){
+                $get_contents = __DIR__ . "/../BaseTestcase/Testcase.php";
+                if(File::exists($get_contents)){
+                    $success = File::copyDirectory($sourceDir, $destinationDir);
+                    file_put_contents($testcase_main,file_get_contents($get_contents));
+                    if($success){
+                        $this->info("Testing directory for local database( Temporary Storage ) is successfully created");
+                    }
+                    else{
+                        $this->error("There was an error in creating local testing folder");
+                    }
+                }
+                else{
+                    $this->error("No file in package directory");
+                }
             }
             else{
-                $this->error("There was an error in creating local testing folder");
+                $this->error("No file in tests directory");
             }
+
         }
         catch(\Exception $e){
             die("The base file doesn't exist");
